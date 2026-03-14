4a1f04318aa1c1e05ad8b550274625088c18e69a
Prof. Renato Oliveira <162147229+profRenatoOliveira1@users.noreply.github.com>
2026-03-14 15:18:05 -0300

    feat: criando método par listar todos os livros

--- src/controller/LivroController.ts
+++ src/controller/LivroController.ts
@@ -2,7 +2,15 @@ import Livro from "../model/Livro.js";
 import { type Request, type Response } from "express";
 
 class LivroController extends Livro {
-    
+    static async todos(req: Request, res: Response) {
+        try {
+            const listaDeLivros = await Livro.listarLivros();
+            return res.status(200).json(listaDeLivros);
+        } catch (error) {
+            console.error(`Erro ao listar livros: ${error}`);
+            return res.status(500).json({ mensagem: "Erro ao recuperar as informações dos livros." });
+        }
+    }
 }
 
 export default LivroController;
